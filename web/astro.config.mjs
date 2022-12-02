19cb5db00a91ec5329fddfb93a9ee1f1d668253a
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 19:48:59 +0100

    netlify stuff

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  output: "server",
+  adapter: netlify()
 });
