03fb2020bcfd75b81d8d6a0f227253fef467468f
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 12:38:02 +0100

    tailwind setup

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
+
+    extend: {
+      colors: {
+        bgBlue: "#151520",
+        greyBlue: "#25323F",
+        blueStroke: "#163451",
+        ctaGreen: "#228945",
+        greenStroke: "#17DB59",
+        white: "#EBF2F5",
+      },
+    },
+  },
+  plugins: [],
+};
