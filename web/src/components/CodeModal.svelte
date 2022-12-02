f461005b5108b2a2f3e33353ec0e48a4cbaa25f5
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 16:41:56 +0100

    modal styling

--- web/src/components/CodeModal.svelte
+++ web/src/components/CodeModal.svelte
@@ -3,20 +3,26 @@
 
 	export let name: string
 	export let code: string
+	export let showModal: boolean
 </script>
 
 <sectio
-	transition:fade
-	class="absolute top-0 left-0 grid w-screen h-screen px-5 backdrop-blur-sm bg-slate-900/20 place-items-center"
+	transition:fade={{ duration: 150 }}
+	class="fixed top-0 left-0 grid w-screen h-screen px-5 backdrop-blur-sm bg-slate-900/20 place-items-center"
 >
-	<div class="flex flex-col items-center justify-center w-full border bg-greyBlue border-blueStroke rounded-xl">
-		<span>{name}</span>
-		<div>
-			<img src={`qr-${code}.png`} alt="${code}" />
+	<div
+		class="flex flex-col items-center justify-center w-full gap-5 px-5 py-4 border bg-greyBlue border-blueStroke rounded-xl"
+	>
+		<span class="text-gray-400 ">{name}</span>
+		<div class="w-full overflow-hidden rounded-xl">
+			<img class="w-full" src={`qr-${code}.png`} alt="${code}" />
 		</div>
 
 		<button
-			class="flex items-center justify-center w-full gap-2 p-3 font-semibold border bg-ctaGreen border-greenStroke rounded-xl tex-xl"
+			on:click={() => {
+				showModal = !showModal
+			}}
+			class="flex items-center justify-center w-full gap-2 p-3 mt-3 font-semibold border bg-ctaGreen border-greenStroke rounded-xl tex-xl"
 		>
 			Stäng
 		</button>
