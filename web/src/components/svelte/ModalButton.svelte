585e46c111bdb1c389979b5eb304aea69a5cde05
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 19:31:10 +0100

    ssr icons and cleanup

--- web/src/components/svelte/ModalButton.svelte
+++ web/src/components/svelte/ModalButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import CodeModal from "./CodeModal.svelte"
+	import Icon from "./Icon.svelte"
 
 	export let name: string
 	export let code: string
@@ -17,6 +18,6 @@
 	}}
 	class="flex items-center justify-center w-full gap-2 p-3 mt-6 font-semibold border bg-ctaGreen border-greenStroke rounded-xl tex-xl"
 >
-	<iconify-icon icon="material-symbols:qr-code-rounded" class="text-2xl" />
+	<Icon name="qrCode" className=" w-7 text-white" />
 	<p>Visa QR</p>
 </button>
