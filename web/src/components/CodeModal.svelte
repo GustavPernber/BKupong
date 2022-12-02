45fa19b4113a1150ab877b22c6b1691ce5f1e49a
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 16:28:39 +0100

    generate qr

--- web/src/components/CodeModal.svelte
+++ web/src/components/CodeModal.svelte
@@ -1,10 +1,24 @@
-<script>
+<script lang="ts">
 	import { fade } from "svelte/transition"
+
+	export let name: string
+	export let code: string
 </script>
 
-<div
+<sectio
 	transition:fade
-	class="absolute top-0 left-0 grid w-screen h-screen backdrop-blur-sm bg-slate-900/20 place-items-center"
+	class="absolute top-0 left-0 grid w-screen h-screen px-5 backdrop-blur-sm bg-slate-900/20 place-items-center"
 >
-	<div>content</div>
-</div>
+	<div class="flex flex-col items-center justify-center w-full border bg-greyBlue border-blueStroke rounded-xl">
+		<span>{name}</span>
+		<div>
+			<img src={`qr-${code}.png`} alt="${code}" />
+		</div>
+
+		<button
+			class="flex items-center justify-center w-full gap-2 p-3 font-semibold border bg-ctaGreen border-greenStroke rounded-xl tex-xl"
+		>
+			Stäng
+		</button>
+	</div>
+</sectio>
