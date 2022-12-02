267955773bb3e2e3b766e093cd1f516cbedeabe5
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 15:59:16 +0100

    some svelte and modal setup

---
+++ web/src/components/CodeModal.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fade } from "svelte/transition"
+</script>
+
+<div
+	transition:fade
+	class="absolute top-0 left-0 grid w-screen h-screen backdrop-blur-sm bg-slate-900/20 place-items-center"
+>
+	<div>content</div>
+</div>
