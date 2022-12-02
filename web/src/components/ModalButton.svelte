8039a16c89ef79efd08ae74307d6285176fa1583
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-02 15:59:16 +0100

    some svelte and modal setup

---
+++ web/src/components/ModalButton.svelte
@@ -0,0 +1,19 @@
+<script>
+	import CodeModal from "./CodeModal.svelte"
+
+	let showModal = true
+</script>
+
+{#if showModal}
+	<CodeModal />
+{/if}
+
+<button
+	on:click={() => {
+		showModal = !showModal
+	}}
+	class="flex items-center justify-center w-full p-3 mt-6 font-semibold border bg-ctaGreen border-greenStroke rounded-xl tex-xl gap-2"
+>
+	<iconify-icon icon="material-symbols:qr-code-rounded" class="text-2xl" />
+	<p>Visa QR</p>
+</button>
