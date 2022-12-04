d6dfbdbcb3e7839bb0aead5dfc739b9996536d97
Gustav Pernber <105298366+GustavPernber@users.noreply.github.com>
2022-12-04 20:07:54 +0100

    maybe pwa implementation?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,53 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
+
+import AstroPWA from "@vite-pwa/astro"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
-});
+	integrations: [
+		tailwind(),
+		svelte(),
+		AstroPWA({
+			// mode: "development",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "BKupong",
+				short_name: "BKupong",
+				theme_color: "#151520",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png"
+					}
+					// {
+					// 	src: "pwa-512x512.png",
+					// 	sizes: "512x512",
+					// 	type: "image/png"
+					// },
+					// {
+					// 	src: "pwa-512x512.png",
+					// 	sizes: "512x512",
+					// 	type: "image/png",
+					// 	purpose: "any maskable"
+					// }
+				]
+			},
+			workbox: {
+				globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"]
+			}
+			// devOptions: {
+			// 	enabled: true
+			// }
+		})
+	]
+})
